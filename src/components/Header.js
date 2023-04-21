62310bd5111c02ec4af473d9e7ff0ae1249b1777
Brishna Bakshev <b.bakshev@gmail.com>
2023-04-21 08:34:17 -0700

    added new files and removed unnecessary files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Header(){
+  return (
+    <React.Fragment>
+      <h1>Inventory Tracker</h1>
+      {/* <img src={ticketsImage} alt="An image of tickets" /> */}
+    </React.Fragment>
+  );
+}
+
+export default Header;
