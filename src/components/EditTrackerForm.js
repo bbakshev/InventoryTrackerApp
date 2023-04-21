30b920e073026684b7f54db621f9a72f5a40cf4b
Brishna Bakshev <b.bakshev@gmail.com>
2023-04-21 11:58:49 -0700

    WIP: lunch, completed edit form

--- src/components/EditTrackerForm.js
+++ src/components/EditTrackerForm.js
@@ -1,3 +1,32 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
+
+function EditTrackerForm (props) {
+  const { tracker } = props;
+
+  function handleEditTrackerFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTracker({
+      names: event.target.names.value, 
+      origin: event.target.origin.value, 
+      price: event.target.price.value, 
+      roast: event.target.roast.value, 
+      id: tracker.id}
+      );
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditTrackerFormSubmission}
+        buttonText="Update Tracker" />
+    </React.Fragment>
+  );
+}
+
+EditTrackerForm.propTypes = {
+  onEditTracker: PropTypes.func
+};
+
+export default EditTrackerForm;
